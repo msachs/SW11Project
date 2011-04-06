d89c24ec2de4fbe11b3504487ab21cac5ffe51e4
urlp <philipp.url@student.tugraz.at>
2011-04-06 14:50:11 +0200

    updated templates

--- public/templates/content_shell.sh
+++ public/templates/content_shell.sh
@@ -1,14 +1,16 @@
+#!<mindshare:content>Interpreter Path</mindshare:content>
 
-# <mindshare:content>Kommentar_1</mindshare:content>
-# <mindshare:content>Kommentar_2</mindshare:content>
-# <mindshare:content>Kommentar_3</mindshare:content>
+# <mindshare:content>Author</mindshare:content>
+# <mindshare:content>Date</mindshare:content>
+# <mindshare:content>Contact</mindshare:content>
+# <mindshare:content>Commantary 1</mindshare:content>
+# <mindshare:content>Commantary 2</mindshare:content>
+# <mindshare:content>Commantary 3</mindshare:content>
 
 echo ""
-echo "<mindshare:content>Echo_1</mindshare:content>"
-echo "<mindshare:content>Echo_1</mindshare:content>"
-echo "<mindshare:content>Echo_1</mindshare:content>"
+echo "<mindshare:content>Echo 1</mindshare:content>"
+echo "<mindshare:content>Echo 2</mindshare:content>"
+echo "<mindshare:content>Echo 3</mindshare:content>"
 echo ""
 
-"<mindshare:content>Command_1</mindshare:content>"
-"<mindshare:content>Command_2</mindshare:content>"
-"<mindshare:content>Command_3</mindshare:content>"
+"<mindshare:content>Commands</mindshare:content>"
