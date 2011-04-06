184fc4fd73d2a9ba04d76701fc6ece0923dabf49
Michael Münzer <michael.muenzer@student.tugraz.at>
2011-04-06 16:28:01 +0200

    Cleaning done

--- app/controllers/Templates.java
+++ app/controllers/Templates.java
@@ -14,19 +14,15 @@ import javax.persistence.EntityManager;
 import models.*;
 
 public class Templates extends Controller {
-    private static String readFileAsString(String filePath)
+    
+	private static String readFileAsString(String filePath)
     throws java.io.IOException{
         StringBuffer fileData = new StringBuffer(1000);
-        
         VirtualFile vf = VirtualFile.fromRelativePath(filePath);
         File realFile = vf.getRealFile();
         FileReader fr = new FileReader(realFile);
-        
-        BufferedReader reader = new BufferedReader(
-                fr);
-        
-
-        
+        BufferedReader reader = new BufferedReader(fr);
+            
         char[] buf = new char[1024];
         int numRead=0;
         while((numRead=reader.read(buf)) != -1){
@@ -45,15 +41,9 @@ public class Templates extends Controller {
 		String filepath = "/public/templates/" + selected_template.filename;
 		String text = readFileAsString(filepath);
 		
-		//File template_file = new File(filepath);
-		//String text = FileFunctions.readTextFile("C:\\temp\\xmas.html");
-		
-		System.out.println(text);
-
-		
-		//System.out.println(selected_template.filename);
-    	// file suchen mit id und filename
-    	// gib methode filename und suche alle strings
+		//System.out.println(text);
+    	// gib methode filename und speichere file in string
+		// filtere string nach auftreten von tags in ArrayList<DataTag>
     	// gib string array an render für ausgabe
     	render(id);
     }
