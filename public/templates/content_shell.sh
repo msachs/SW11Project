5e15f03202b3aa0b136506d4437149c813d501ef
urlp <philipp.url@student.tugraz.at>
2011-04-06 13:44:58 +0200

    cv, sh, makefile templates

---
+++ public/templates/content_shell.sh
@@ -0,0 +1,14 @@
+
+# <mindshare:content>Kommentar_1</mindshare:content>
+# <mindshare:content>Kommentar_2</mindshare:content>
+# <mindshare:content>Kommentar_3</mindshare:content>
+
+echo ""
+echo "<mindshare:content>Echo_1</mindshare:content>"
+echo "<mindshare:content>Echo_1</mindshare:content>"
+echo "<mindshare:content>Echo_1</mindshare:content>"
+echo ""
+
+"<mindshare:content>Command_1</mindshare:content>"
+"<mindshare:content>Command_2</mindshare:content>"
+"<mindshare:content>Command_3</mindshare:content>"
