bb6c08812d9ebb97281375a84e0f757d03538102
urlp <philipp.url@student.tugraz.at>
2011-05-18 15:25:16 +0200

    removed duplicate data_tags

--- app/controllers/Templates.java
+++ app/controllers/Templates.java
@@ -24,85 +24,110 @@ import models.*;
 import Support.*;
 
 public class Templates extends Controller {
-    
-	public static ArrayList<DataTag> searchDataTags(String file_content)
-	{
+
+	public static ArrayList<DataTag> searchDataTags(String file_content) {
 		ArrayList<DataTag> data_tags = new ArrayList<DataTag>();
-		
+
 		// init tags and offsets
 		String MINDSHARE_BEGIN_TAG = "<mindshare:";
-	    String MINDSHARE_END_TAG = "</mindshare:";
-	    String MINDSHARE_CLOSE_TAG = ">";
+		String MINDSHARE_END_TAG = "</mindshare:";
+		String MINDSHARE_CLOSE_TAG = ">";
 		Integer offset_start_begin_tag = 0;
 		Integer offset_start_end_tag = 0;
 		Integer offset_close_begin_tag = 0;
 		Integer offset_close_end_tag = 0;
-		
-		offset_start_begin_tag = file_content.indexOf(MINDSHARE_BEGIN_TAG, offset_start_end_tag);
-		offset_start_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG, offset_start_begin_tag);
-		offset_close_begin_tag = file_content.indexOf(MINDSHARE_END_TAG, offset_close_end_tag);
-		offset_close_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG, offset_close_begin_tag);
+
+		offset_start_begin_tag = file_content.indexOf(MINDSHARE_BEGIN_TAG,
+				offset_start_end_tag);
+		offset_start_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG,
+				offset_start_begin_tag);
+		offset_close_begin_tag = file_content.indexOf(MINDSHARE_END_TAG,
+				offset_close_end_tag);
+		offset_close_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG,
+				offset_close_begin_tag);
 
 		// run through the whole string and search for tags
-		while (offset_start_begin_tag != -1 && (offset_close_begin_tag > offset_start_end_tag))
-		{
-			String start_tag_type = file_content.substring(offset_start_begin_tag+MINDSHARE_BEGIN_TAG.length(), offset_start_end_tag);
-			while(start_tag_type.length() <= 0)
-			{
+		while (offset_start_begin_tag != -1
+				&& (offset_close_begin_tag > offset_start_end_tag)) {
+			String start_tag_type = file_content.substring(
+					offset_start_begin_tag + MINDSHARE_BEGIN_TAG.length(),
+					offset_start_end_tag);
+			while (start_tag_type.length() <= 0) {
 
-				offset_start_begin_tag = file_content.indexOf(MINDSHARE_BEGIN_TAG, offset_start_end_tag);
-				offset_start_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG, offset_start_begin_tag);
-				start_tag_type = file_content.substring(offset_start_begin_tag+MINDSHARE_BEGIN_TAG.length(), offset_start_end_tag);
+				offset_start_begin_tag = file_content.indexOf(
+						MINDSHARE_BEGIN_TAG, offset_start_end_tag);
+				offset_start_end_tag = file_content.indexOf(
+						MINDSHARE_CLOSE_TAG, offset_start_begin_tag);
+				start_tag_type = file_content.substring(offset_start_begin_tag
+						+ MINDSHARE_BEGIN_TAG.length(), offset_start_end_tag);
 			}
-			
-			String end_tag_type = file_content.substring(offset_close_begin_tag+MINDSHARE_END_TAG.length(),offset_close_end_tag);
-			String description = file_content.substring(offset_start_end_tag+1, offset_close_begin_tag);
-			
-			
-			//System.out.println(start_tag_type.length());
-			
+
+			String end_tag_type = file_content.substring(offset_close_begin_tag
+					+ MINDSHARE_END_TAG.length(), offset_close_end_tag);
+			String description = file_content.substring(
+					offset_start_end_tag + 1, offset_close_begin_tag);
+
+			// System.out.println(start_tag_type.length());
+
 			// create tags and save them into an array list
-			if ((start_tag_type.equals(end_tag_type)))
-			{	
-				if (start_tag_type.equals("content"))
-				{
-					DataTag actual_tag = new DataTag(TagTyp.CONTENT, description);
-					data_tags.add(actual_tag);	
-				}
-				else if(start_tag_type.equals("date"))
-				{
-					DataTag actual_tag = new DataTag(TagTyp.DATE, description);
-					data_tags.add(actual_tag);	
-				}
-				else if(start_tag_type.equals("text_short"))
-				{
-					DataTag actual_tag = new DataTag(TagTyp.TEXT_SHORT, description);
-					data_tags.add(actual_tag);	
-				}
-				else if(start_tag_type.equals("text_long"))
-				{
-					DataTag actual_tag = new DataTag(TagTyp.TEXT_LONG, description);
-					data_tags.add(actual_tag);	
-				}
-				//TODO: perform error handling for tagtype which does not exist
-			}
-			
-			// increment offset for tags
-			offset_start_begin_tag = file_content.indexOf(MINDSHARE_BEGIN_TAG, offset_close_end_tag);
-			offset_start_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG, offset_start_begin_tag);
-			offset_close_begin_tag = file_content.indexOf(MINDSHARE_END_TAG, offset_close_end_tag);
-			offset_close_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG, offset_close_begin_tag);
 
-/*			System.out.println("start begin: " +offset_start_begin_tag);
-			System.out.println("start end: " +offset_start_end_tag);
-			System.out.println("close begin: " +offset_close_begin_tag);
-			System.out.println("close end: " +offset_close_end_tag);*/	
+			if ((start_tag_type.equals(end_tag_type))) {
+
+				if (start_tag_type.equals("content")) {
+					DataTag actual_tag = new DataTag(TagTyp.CONTENT,
+							description);
+
+					if (!checkDuplicateDataTags(actual_tag, data_tags)) {
+						data_tags.add(actual_tag);
+					}
+
+				} else if (start_tag_type.equals("date")) {
+					DataTag actual_tag = new DataTag(TagTyp.DATE, description);
+
+					if (!checkDuplicateDataTags(actual_tag, data_tags)) {
+						data_tags.add(actual_tag);
+					}
+				} else if (start_tag_type.equals("text_short")) {
+					DataTag actual_tag = new DataTag(TagTyp.TEXT_SHORT,
+							description);
+
+					if (!checkDuplicateDataTags(actual_tag, data_tags)) {
+						data_tags.add(actual_tag);
+					}
+
+				} else if (start_tag_type.equals("text_long")) {
+					DataTag actual_tag = new DataTag(TagTyp.TEXT_LONG,
+							description);
+
+					if (!checkDuplicateDataTags(actual_tag, data_tags)) {
+						data_tags.add(actual_tag);
+					}
+				}
+				// TODO: perform error handling for tagtype which does not exist
+			}
+
+			// increment offset for tags
+			offset_start_begin_tag = file_content.indexOf(MINDSHARE_BEGIN_TAG,
+					offset_close_end_tag);
+			offset_start_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG,
+					offset_start_begin_tag);
+			offset_close_begin_tag = file_content.indexOf(MINDSHARE_END_TAG,
+					offset_close_end_tag);
+			offset_close_end_tag = file_content.indexOf(MINDSHARE_CLOSE_TAG,
+					offset_close_begin_tag);
+
+			/*
+			 * System.out.println("start begin: " +offset_start_begin_tag);
+			 * System.out.println("start end: " +offset_start_end_tag);
+			 * System.out.println("close begin: " +offset_close_begin_tag);
+			 * System.out.println("close end: " +offset_close_end_tag);
+			 */
 		}
-		  
-	    //file_content.substring(MINDSHARE_OPEN_TAG, ">");			
+
+		// file_content.substring(MINDSHARE_OPEN_TAG, ">");
 		return data_tags;
 	}
-	
+
 	public static void show(Long id) {
 
 		Template selected_template = Template.find("byId", id).first();
@@ -111,76 +136,75 @@ public class Templates extends Controller {
 		try {
 			file_content = selected_template.getTemplate();
 			ArrayList<DataTag> data_tags = new ArrayList<DataTag>();
-	        data_tags = searchDataTags(file_content);
+			data_tags = searchDataTags(file_content);
 
 			// filtere string nach auftreten von tags in ArrayList<DataTag>
-	    	// gib string array an render für ausgabe
-	    	render(id, name, data_tags);
+			// gib string array an render für ausgabe
+			render(id, name, data_tags);
 		} catch (IOException e) {
-			
+
 			render(e.getMessage());
 		}
-    }
-	
-	public static void generateFile(Long id, ArrayList<String> data_tags){
-		
+	}
+
+	public static void generateFile(Long id, ArrayList<String> data_tags) {
+
 		Template selected_template = Template.find("byId", id).first();
 		String file_content;
 		try {
 			file_content = selected_template.getTemplate();
 			ArrayList<DataTag> result_data_tags = new ArrayList<DataTag>();
-	        result_data_tags = searchDataTags(file_content);
-			
-			for(int count = 0; count < data_tags.size(); count++)
-			{
+			result_data_tags = searchDataTags(file_content);
+
+			for (int count = 0; count < data_tags.size(); count++) {
 				result_data_tags.get(count).setContent(data_tags.get(count));
 			}
-			
-			DocumentGenerator doc_gen = new DocumentGenerator(file_content, result_data_tags);
+
+			DocumentGenerator doc_gen = new DocumentGenerator(file_content,
+					result_data_tags);
 			String result = doc_gen.getResult();
-			
-			if (selected_template.isMultifile())
-			{
+
+			if (selected_template.isMultifile()) {
 				ArrayList<NamedString> named_strings = new ArrayList<NamedString>();
 				int index = 0;
-				while (result.indexOf("?mindshare|fileend", index) > -1)
-				{
-					int new_index = result.indexOf("?mindshare|fileend*", index);
-					
+				while (result.indexOf("?mindshare|fileend", index) > -1) {
+					int new_index = result
+							.indexOf("?mindshare|fileend*", index);
+
 					int name_index = result.indexOf("*", new_index) + 1;
 					int name_end = result.indexOf("*", name_index);
-					
-					String name = result.substring(name_index , name_end);
-					
+
+					String name = result.substring(name_index, name_end);
+
 					System.out.println("Name: " + name);
-					
-					named_strings.add(new NamedString(name, result.substring(index, new_index)));
-					
+
+					named_strings.add(new NamedString(name, result.substring(
+							index, new_index)));
+
 					// To prevent endless loops
-				    new_index++;
-					
+					new_index++;
+
 					index = name_end + 2;
-					
-					System.out.println("Index: " + index + " Newindex: " + new_index);
+
+					System.out.println("Index: " + index + " Newindex: "
+							+ new_index);
 				}
-				
-				ZipFactory.Generate(named_strings, selected_template.filename, 
-						"application/zip", false);				
-			}
-			else
-			{			
-			    InputStream file_stream = new ByteArrayInputStream(result.getBytes());
+
+				ZipFactory.Generate(named_strings, selected_template.filename,
+						"application/zip", false);
+			} else {
+				InputStream file_stream = new ByteArrayInputStream(
+						result.getBytes());
 				response.setContentTypeIfNotSet("text/plain; charset=utf-8");
-				renderBinary(file_stream, selected_template.getFilename()); 
+				renderBinary(file_stream, selected_template.getFilename());
 			}
 		} catch (IOException e) {
-			
+
 			render(e.getMessage());
 		}
 
-		
-		//datatags.content = data_tags.get(0);
-		//DocumentGenerator("",data_tags);
+		// datatags.content = data_tags.get(0);
+		// DocumentGenerator("",data_tags);
 	}
 
 	public static void download(Long id) {
@@ -189,17 +213,27 @@ public class Templates extends Controller {
 
 		try {
 			file_content = selected_template.getTemplate();
-			InputStream file_stream = new ByteArrayInputStream(file_content.getBytes());
-			
+			InputStream file_stream = new ByteArrayInputStream(
+					file_content.getBytes());
+
 			response.setContentTypeIfNotSet("text/plain");
 			renderBinary(file_stream, selected_template.getFilename());
 		} catch (IOException e) {
-			
+
 			render(e.getMessage());
 		}
 
-	
-    }
+	}
 
-	
+	private static boolean checkDuplicateDataTags(DataTag actual_tag,
+			ArrayList<DataTag> data_tags) {
+		for (int i = 0; i < data_tags.size(); i++) {
+			if (actual_tag.getDescription().equals(
+					data_tags.get(i).getDescription())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
