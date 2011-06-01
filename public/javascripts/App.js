00e46514f540247b5a752856ec83b70a2be93ea4
Christoph Pojer <christoph.pojer@gmail.com>
2011-06-01 11:38:04 +0200

    Moar magic!

--- public/javascripts/App.js
+++ public/javascripts/App.js
@@ -7,16 +7,34 @@ var preventDefault = function(event){
 
 var attachDatePicker = function(){
 
-	$$('a.date').addEvent('click', preventDefault);
+	var anchors = document.getElements('a.date');
 	
-	new Picker.Date($$('input.date'), {
-		toggle: $$('a.date'),
+	anchors.addEvent('click', preventDefault);
+	new Picker.Date(document.getElements('input.date'), {
+		toggle: anchors,
 		positionOffset: {x: 5, y: 0},
 		pickerClass: 'datepicker_vista'
 	});
 
-}
+};
 
-window.addEvent('domready', attachDatePicker);
+var listener = function(event){
+	event.preventDefault();
+	
+	document.location = this.get('data-href').substitute({id: document.id('templateChooser').get('value')});
+};
+
+var attachTemplateChooser = function(){
+	
+	document.getElements('div.buttons button.download, div.buttons button.proceed').addEvent('click', listener);
+	
+};
+
+
+window.addEvent('domready', function(){
+	attachDatePicker();
+	
+	attachTemplateChooser();
+});
 
 })();
