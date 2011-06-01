07b6c6882da2847ce0e2ce8236c4b2b0236f7ca4
Christoph Pojer <christoph.pojer@gmail.com>
2011-06-01 11:13:31 +0200

    Date picker added

--- public/javascripts/App.js
+++ public/javascripts/App.js
@@ -1,6 +1,22 @@
 // packager build Core/DOMReady MooTools-DatePicker/Picker.Date > mootools.js
 (function(){
 
+var preventDefault = function(event){
+	event.preventDefault();
+}
 
+var attachDatePicker = function(){
+
+	$$('a.date').addEvent('click', preventDefault);
+	
+	new Picker.Date($$('input.date'), {
+		toggle: $$('a.date'),
+		positionOffset: {x: 5, y: 0},
+		pickerClass: 'datepicker_vista'
+	});
+
+}
+
+window.addEvent('domready', attachDatePicker);
 
 })();
